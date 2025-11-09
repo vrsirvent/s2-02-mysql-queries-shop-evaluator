b3e91cf346dc07468bd5429fd9161e942af42c09
vrsirvent <vrsirvent@gmail.com>
2025-11-09 12:02:33 +0100

    Update queries.sql
    Update 2

--- queries/queries.sql
+++ queries/queries.sql
@@ -26,7 +26,7 @@ SELECT nombre, UPPER(SUBSTRING(nombre, 1, 2)) AS 'iniciales' FROM fabricante;
 SELECT nombre, ROUND(precio) AS 'precio' FROM producto;
 
 -- 10. Llista els noms i els preus de tots els productes (precio truncado) de la taula producto, truncant el valor del preu per a mostrar-lo sense cap xifra decimal.
-SELECT nombre, TRUNCATE(precio, 0) AS "precio truncado" FROM producto;
+SELECT nombre, TRUNCATE(precio, 0) AS 'precio truncado' FROM producto;
 
 -- 11. Mostra una llista amb els codis dels fabricants que apareixen a la taula producto, incloent possibles repeticions.
 
