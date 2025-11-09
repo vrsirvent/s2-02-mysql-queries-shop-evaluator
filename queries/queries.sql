14a53ba2b3f9cf9a597b358269729ee630e8acf2
vrsirvent <vrsirvent@gmail.com>
2025-11-09 12:16:17 +0100

    Update queries.sql

--- queries/queries.sql
+++ queries/queries.sql
@@ -68,7 +68,7 @@ SELECT producto.nombre, producto.precio, fabricante.nombre AS 'nombre del fabric
 SELECT producto.codigo, producto.nombre, producto.codigo_fabricante AS 'codigo fabricante', fabricante.nombre AS 'nombre fabricante' FROM producto JOIN fabricante ON producto.codigo_fabricante = fabricante.codigo;
 
 -- 24. Retorna el nom, el preu i el nom del fabricant (fabricante), del producte més barat.
-  SELECT producto.nombre, producto.precio, fabricante.nombre AS 'fabricante' FROM producto JOIN fabricante ON producto.codigo_fabricante = fabricante.codigo ORDER BY producto.precio ASC LIMIT 1;
+SELECT producto.nombre, producto.precio, fabricante.nombre AS 'fabricante' FROM producto JOIN fabricante ON producto.codigo_fabricante = fabricante.codigo ORDER BY producto.precio ASC LIMIT 1;
 
 -- 25. Retorna el nom del producte, el preu i el nom del seu fabricant (fabricante), del producte més car.
 SELECT producto.nombre, producto.precio, fabricante.nombre AS 'fabricante' FROM producto JOIN fabricante ON producto.codigo_fabricante = fabricante.codigo ORDER BY producto.precio DESC LIMIT 1; 
